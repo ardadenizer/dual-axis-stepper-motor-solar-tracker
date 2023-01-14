675083b9d058d69d2c56e7a115bacca8c0fe1934
Arda Denizer <ardadenizer95@gmail.com>
2023-01-14 16:03:50 +0200

    swing with push button stop

--- dual axis stepper motor control for solar tracking/src/main.cpp
+++ dual axis stepper motor control for solar tracking/src/main.cpp
@@ -1,9 +1,51 @@
 #include <Arduino.h>
+#include <AccelStepper.h>
+// Define stepper motor connections and motor interface type. Motor interface type must be set to 1 when using a driver:
+#define dirPin 4
+#define stepPin 5
+#define motorInterfaceType 1
+
+int state = true;
+int value = digitalRead(2);
+
+// Definition of function prototypes:
+void switchInterrupt (void);
+
+// Create a new instance of the AccelStepper class:
+AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 
 void setup() {
-  // put your setup code here, to run once:
+  // Set the maximum speed and acceleration:
+  stepper.setMaxSpeed(1000);
+  stepper.setAcceleration(500);
+  stepper.setSpeed(50);
+  attachInterrupt(digitalPinToInterrupt(2), switchInterrupt, FALLING);
+  Serial.begin(9600);
+  pinMode(2,INPUT_PULLUP);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while (state){
+    // Set the target position:
+    Serial.println(value);
+    
+    stepper.moveTo(3000);
+    // Run to target position with set speed and acceleration/deceleration:
+    stepper.runToPosition();
+
+    delay(100);
+
+    // Move back to zero:
+    stepper.moveTo(0);
+    stepper.runToPosition();
+
+    delay(100);
+  }
 }
+void switchInterrupt (){
+  stepper.stop();
+  //delay(1000);
+  state = false;
+
+  }
