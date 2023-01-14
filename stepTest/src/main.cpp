8357d27fda95407be6bba39c33a904fdb3f9ca57
Arda Denizer <ardadenizer95@gmail.com>
2023-01-14 20:35:08 +0200

    reverse direction and stopping when it finds the sw

--- stepTest/src/main.cpp
+++ stepTest/src/main.cpp
@@ -1,30 +1,39 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
 
+
 #define dirPin 4
 #define stepPin 5
 #define motorInterfaceType 1
 
+long int number = -1000;
 
-AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
-
+ AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 
 void setup() {
   // put your setup code here, to run once:
-    stepper.setMaxSpeed(1000);
-    stepper.setAcceleration(100);
-    stepper.setSpeed(50);
+ 
+
+  stepper.setMaxSpeed(1000);
+  stepper.setAcceleration(500);
+  stepper.setSpeed(50);
+
+  attachInterrupt(digitalPinToInterrupt(2), switchInterrupt, FALLING);
+
+  pinMode(2,INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    stepper.moveTo(-1000);
-    // Run to target position with set speed and acceleration/deceleration:
-    stepper.runToPosition();
+  stepper.moveTo(number);
+  stepper.runToPosition();
+  stepper.stop();
+}
 
-    delay(100);
+void switchInterrupt(){
 
-    // Move back to zero:
-    // stepper.moveTo(0);
-    // stepper.runToPosition();
+  stepper.stop();
+  stepper.setCurrentPosition(0);
+  number = 0;
+  
 }
