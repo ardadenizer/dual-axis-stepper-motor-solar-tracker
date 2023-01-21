7281a0a21c069e05a3003c417145466181280c27
Arda Denizer <ardadenizer95@gmail.com>
2023-01-21 20:55:27 +0200

    add voltage measurement with dual axis motor ctrl

--- dual axis stepper motor control for solar tracking/src/main.cpp
+++ dual axis stepper motor control for solar tracking/src/main.cpp
@@ -6,18 +6,34 @@ const int topRight = A4;
 const int topLeft = A1; // Top left LDR
 const int bottomRight = A3;
 const int bottomLeft = A2;
+const int voltPin = A0;
  
 const int maxStepCountVertical = 1160; // maximum step count limit for the stepper motor
 const int maxStepCountHorizontal = 500;
 int stepCountVertical = 0; // current step count
 int stepCountHorizontal = 0;
 
+// Definitions for voltage measurement
+float adcVoltage = 0;
+float inputVoltage = 0;
+
+// Resistor value definitions
+
+float R1 = 30000.0;
+float R2 = 7500.0;
+
+float refVoltage = 5.0;
+
+
+
+
 Stepper motorVertical(stepsPerRevolution,8,9); // create a stepper object
 Stepper motorHorizontal(stepsPerRevolution,4,5); // create a stepper object
 
 void setup() {
-  motorVertical.setSpeed(70); // set the speed of the stepper motor
-  motorHorizontal.setSpeed(70);
+  motorVertical.setSpeed(200); // set the speed of the stepper motor
+  motorHorizontal.setSpeed(200);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -28,6 +44,15 @@ void loop() {
 
   volatile int verticalDiff = (topRightVal + bottomRightVal) - (topLeftVal + bottomLeftVal);
   volatile int horizontalDiff = (topRightVal + topLeftVal) - (bottomRightVal + bottomLeftVal); 
+
+  volatile int  adcValue = analogRead(voltPin);
+
+  adcVoltage = (adcValue * refVoltage) / 1024.0;
+  inputVoltage = adcVoltage / (R2 / (R1 + R2));
+
+  Serial.print("Input voltage =");
+  Serial.println(inputVoltage,2);
+
   
 
    if (horizontalDiff > 0 && stepCountHorizontal < maxStepCountHorizontal) {
